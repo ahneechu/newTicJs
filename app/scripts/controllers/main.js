d53e2a9549aab0f10c2549c2b7ad7f8e65e2b749
Anne Chu <ahnee.chu@gmail.com>
2013-10-09 16:46:21 -0700

    added start, reset and play again btns.  updated to firebase version

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,77 +1,104 @@
 'use strict';
 
-// angular.module('newTicApp')
-//   .controller('MainCtrl', function ($scope) {
-//     $scope.awesomeThings = [
-//       'HTML5 Boilerplate',
-//       'AngularJS',
-//       'Karma'
-//     ];
-//   });
+angular.module('newTicApp')
+  .controller('ticTacCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
 
-function ticTacCtrl($scope){
+	var url = new Firebase("https://newtictactoe.firebaseio.com/games");
+  	$scope.games={};
+  	var promise = angularFire(url, $scope, "games");
 
-$scope.gameBoard= ['','','','','','','','',''];
+    promise.then(function(){
+    	$scope.games = {
+    		gameBoard: ['','','','','','','','',''],
+    		playerTurn: 1,
+			gameEnded: false
+    	};
+  
 
-var playerTurn = 1;
+    	$scope.clickBox = function(box){
+			console.log("line 19")
+			if ($scope.games.gameEnded == true) 
+			return;
 
-var gameEnded = false;
+			if($scope.games.gameBoard[box] !='')
+				// alert ("pick another box!");
+			return;
 
-$scope.clickBox = function(box){
+				if($scope.games.playerTurn % 2 == 1)
+					$scope.games.gameBoard[box] = "X";	
+				else
+					$scope.games.gameBoard[box] ="O";
 
-	if (gameEnded == true) 
-	return;
+				$scope.games.playerTurn++;
 
-	if($scope.gameBoard[box] !='')
-		// alert ("pick another box!");
-	return;
-
-		if(playerTurn % 2 == 1)
-			$scope.gameBoard[box] = "X";	
-		else
-			$scope.gameBoard[box] ="O";
-
-		playerTurn++;
-
-		console.log($scope.gameBoard[box])
-		console.log($scope.gameBoard)
+				console.log($scope.games.gameBoard[box])
+				console.log($scope.games.gameBoard)
 // alert(box);	
+		};
 
-	}
 
-$scope.win = function(){
-	var wins=[[0, 1, 2, "row 1"],[3, 4, 5, "row 2"],[6, 7, 8, "row 3"],
-			 [0, 3, 6, "column 1"],[1, 4, 7, "column 2"],[2, 5, 8, "column 3"],
-			 [0, 4, 8, "diagonal 1"],[2, 4, 6, "diagonal 2"]]
+		$scope.win = function(){
+			var wins=[[0, 1, 2, "row 1"],[3, 4, 5, "row 2"],[6, 7, 8, "row 3"],
+					 [0, 3, 6, "column 1"],[1, 4, 7, "column 2"],[2, 5, 8, "column 3"],
+					 [0, 4, 8, "diagonal 1"],[2, 4, 6, "diagonal 2"]];
 
 	
 			for(var i = 0; i < wins.length; i++)
-				if (($scope.gameBoard[wins[i][0]] != '') && 
-					($scope.gameBoard[wins[i][0]] == $scope.gameBoard[wins[i][1]]) && 
-					($scope.gameBoard[wins[i][1]] == $scope.gameBoard[wins[i][2]]))
+				if (($scope.games.gameBoard[wins[i][0]] != '') && 
+					($scope.games.gameBoard[wins[i][0]] == $scope.games.gameBoard[wins[i][1]]) && 
+					($scope.games.gameBoard[wins[i][1]] == $scope.games.gameBoard[wins[i][2]]))
 					{
-				
-						alert($scope.gameBoard[wins[i][0]] + " win! by " + wins[i][3]);
-						gameEnded = true;
+						$scope.winMsg = $scope.games.gameBoard[wins[i][0]] + " win! by " + wins[i][3];
+						// alert($scope.gameBoard[wins[i][0]] + " win! by " + wins[i][3]);
+						$scope.games.gameEnded = true;
 					} 				
-	}
-$scope.tie = function(){
-	// gameEnded = true;
+		};
 
-	for(var i = 0; i < $scope.gameBoard.length; i++)
-		if ($scope.gameBoard[i]=="")
-		{
-			gameEnded = false;
-			break;
-		}
+	
+		$scope.winMsg = "";
 
-		if (gameEnded)
-		{
-			alert("it's a tie!");
-			gameEnded = true;
-		}
+		$scope.tie = function(){
+			$scope.games.gameEnded = true;
 
-}
+			for(var i = 0; i < $scope.games.gameBoard.length; i++)
+				if ($scope.games.gameBoard[i]=="")
+				{
+					$scope.games.gameEnded = false;
+					break;
+				}
 
-}
+				if ($scope.games.gameEnded)
+				{
+					$scope.winMsg = "It's a tie!";
+					// alert("it's a tie!");
+					$scope.games.gameEnded = true;
+				}
+		};
+
+		$scope.resetClick = function(){
+			// var box = $scope.gameBoard[box]; 
+			for(var i = 0; i < $scope.games.gameBoard.length; i++){
+				$scope.games.gameBoard[i] = '';	
+			}
+			// gameBoard = ['','','','','','','','',''];
+			$scope.games.playerTurn = 1;
+			console.log($scope.games.gameBoard);
+			// alert( "my button was clicked!");
+		};
+
+		$scope.playAgain = function(){
+			// var box = $scope.gameBoard[box]; 
+			for(var i = 0; i < $scope.games.gameBoard.length; i++){
+				$scope.games.gameBoard[i] = '';	
+			}
+			// gameBoard = ['','','','','','','','',''];
+			$scope.winMsg = "";
+			$scope.games.playerTurn = 1;
+			console.log($scope.games.gameBoard);
+			// alert( "my button was clicked!");
+		};
+
+	});
+
+}]);
 	

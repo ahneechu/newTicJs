e42654b506ea5206431bc99499333f6f9d013715
Anne Chu <ahnee.chu@gmail.com>
2013-10-10 12:43:50 -0700

    updated code with new show win msg and tore and put all js code back together

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,96 +4,117 @@ angular.module('newTicApp')
   .controller('ticTacCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
 
 	var url = new Firebase("https://newtictactoe.firebaseio.com/games");
-  	$scope.games={};
-  	var promise = angularFire(url, $scope, "games");
+  	var promise = angularFire(url, $scope, "games", {});
 
     promise.then(function(){
     	$scope.games = {
+			gameEnded: false,
     		gameBoard: ['','','','','','','','',''],
-    		playerTurn: 1,
-			gameEnded: false
+    		playerTurn: 1
     	};
-  
 
     	$scope.clickBox = function(box){
-			console.log("line 19")
-			if ($scope.games.gameEnded == true) 
-			return;
+    		if ($scope.games.gameBoard[box] != "X" && $scope.games.gameBoard[box] != "O") 
+	    		{
+	    		$scope.games.gameBoard[box] = ($scope.games.playerTurn % 2 == 1 ? "X" : "O");
+	    		}
+			// if($scope.games.gamegameEnded == true) 
+			// return;
 
-			if($scope.games.gameBoard[box] !='')
-				// alert ("pick another box!");
-			return;
+			// if($scope.games.gameBoard[box] !='')
+			// 	// alert ("pick another box!");
+			// return;
 
-				if($scope.games.playerTurn % 2 == 1)
-					$scope.games.gameBoard[box] = "X";	
-				else
-					$scope.games.gameBoard[box] ="O";
+			// 	if($scope.games.playerTurn % 2 == 1)
+			// 		$scope.games.gameBoard[box] = "X";	
+			// 	else
+			// 		$scope.games.gameBoard[box] ="O";
 
+
+				$scope.win();
+				$scope.tie();
 				$scope.games.playerTurn++;
 
-				console.log($scope.games.gameBoard[box])
-				console.log($scope.games.gameBoard)
-// alert(box);	
+				console.log($scope.games.gameBoard[box]);
+				console.log($scope.games.gameBoard);
+
+	  // 	$scope.$watch("games.gameBoard", function(){
+   //  		console.log("Game Ended: " + $scope.games.gameEnded);
+   //  			$scope.win();
+			// 	$scope.tie();
+			// 	$scope.games.playerTurn++;
+    			
+			// console.log("Game Ended: " + $scope.games.gameEnded);
+    	
+   //  	}, true);	// Let's try "deep linking"
+
 		};
+
+		
+	var wins=[[0, 1, 2, "row 1"],[3, 4, 5, "row 2"],[6, 7, 8, "row 3"],
+			 [0, 3, 6, "column 1"],[1, 4, 7, "column 2"],[2, 5, 8, "column 3"],
+			 [0, 4, 8, "diagonal 1"],[2, 4, 6, "diagonal 2"]];
 
 
 		$scope.win = function(){
-			var wins=[[0, 1, 2, "row 1"],[3, 4, 5, "row 2"],[6, 7, 8, "row 3"],
-					 [0, 3, 6, "column 1"],[1, 4, 7, "column 2"],[2, 5, 8, "column 3"],
-					 [0, 4, 8, "diagonal 1"],[2, 4, 6, "diagonal 2"]];
-
-	
-			for(var i = 0; i < wins.length; i++)
-				if (($scope.games.gameBoard[wins[i][0]] != '') && 
+				for(var i = 0; i < wins.length; i++)
+				if(($scope.games.gameBoard[wins[i][0]] != '') && 
 					($scope.games.gameBoard[wins[i][0]] == $scope.games.gameBoard[wins[i][1]]) && 
 					($scope.games.gameBoard[wins[i][1]] == $scope.games.gameBoard[wins[i][2]]))
 					{
-						$scope.winMsg = $scope.games.gameBoard[wins[i][0]] + " win! by " + wins[i][3];
-						// alert($scope.gameBoard[wins[i][0]] + " win! by " + wins[i][3]);
+						// $scope.showWinMsg = $scope.games.gameBoard[wins[i][0]] + " win! by " + wins[i][3];
 						$scope.games.gameEnded = true;
-					} 				
+						$scope.showWinMsg = true;
+						// alert($scope.gameBoard[wins[i][0]] + " win! by " + wins[i][3]);
+						console.log($scope.games.gameEnded);
+						// This is just as a test...
+						$scope.showWinMsg = true;
+						console.log($scope.showWinMsg);
+					}
 		};
 
-	
-		$scope.winMsg = "";
 
 		$scope.tie = function(){
-			$scope.games.gameEnded = true;
+			var mightBeEnded = true;
 
 			for(var i = 0; i < $scope.games.gameBoard.length; i++)
-				if ($scope.games.gameBoard[i]=="")
+				if($scope.games.gameBoard[i]=="")
 				{
-					$scope.games.gameEnded = false;
+					mightBeEnded = false;
 					break;
 				}
 
-				if ($scope.games.gameEnded)
+				$scope.games.gameEnded = mightBeEnded;
+
+				if($scope.games.gameEnded)
 				{
-					$scope.winMsg = "It's a tie!";
+					$scope.showTieMsg = true;
+					console.log($scope.games.gameEnded);
+					console.log($scope.showTieMsg);
 					// alert("it's a tie!");
-					$scope.games.gameEnded = true;
 				}
 		};
 
+
+
 		$scope.resetClick = function(){
-			// var box = $scope.gameBoard[box]; 
 			for(var i = 0; i < $scope.games.gameBoard.length; i++){
 				$scope.games.gameBoard[i] = '';	
 			}
-			// gameBoard = ['','','','','','','','',''];
 			$scope.games.playerTurn = 1;
+			$scope.games.gameEnded = false;
 			console.log($scope.games.gameBoard);
 			// alert( "my button was clicked!");
 		};
 
 		$scope.playAgain = function(){
-			// var box = $scope.gameBoard[box]; 
 			for(var i = 0; i < $scope.games.gameBoard.length; i++){
 				$scope.games.gameBoard[i] = '';	
 			}
-			// gameBoard = ['','','','','','','','',''];
-			$scope.winMsg = "";
+			$scope.showWinMsg = false;
+			$scope.showTieMsg = false;
 			$scope.games.playerTurn = 1;
+			$scope.games.gameEnded = false;
 			console.log($scope.games.gameBoard);
 			// alert( "my button was clicked!");
 		};
@@ -101,4 +122,25 @@ angular.module('newTicApp')
 	});
 
 }]);
+
+	// $scope.showWinMsg = function(winMsg){
+		// 	console.log("line 47")
+		// 	// $scope.win ? true : false;
+
+		// 	if ($scope.win == true)
+		// 	{
+		// 		console.log("line 50")
+		// 		winMsg = $scope.games.gameBoard[wins[i][0]] + " win! by " + wins[i][3];
+		// 		// $scope.games.gameEnded;
+		// 		console.log("line 53")
+		// 	}
+		// 	console.log("line 55")
+		// 	if($scope.tie == true)
+		// 	{
+		// 		console.log("line 58")
+		// 		winMsg = "it's a tie!"
+		// 		// $scope.games.gameEnded;
+		// 		console.log("line 61")
+		// 	}
+		// };
 	

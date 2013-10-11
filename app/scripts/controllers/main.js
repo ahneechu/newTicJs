98c7392a754c0edcf23316df0b9428d614b6d85e
Anne Chu <ahnee.chu@gmail.com>
2013-10-10 19:20:18 -0700

    added queue function and updated code to work with queue

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,21 +2,35 @@
 
 angular.module('newTicApp')
   .controller('ticTacCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
+  	$scope.games=[];
+  	$scope.queue = {};
 
-	var url = new Firebase("https://newtictactoe.firebaseio.com/games");
-  	var promise = angularFire(url, $scope, "games", {});
+  	// $scope.player = '';
+  	// $scope.gameId = -1;
 
-    promise.then(function(){
+  	var games = new Firebase("https://newtictactoe.firebaseio.com/games");
+  	angularFire(games, $scope, "games").then(function(){  
+
+	  	// 
+// });
+
+
+	// var games = new Firebase("https://newtictactoe.firebaseio.com/games");
+ //  	var promise = angularFire(games, $scope, "games", {});
+
+   //  promise.then(function(){
     	$scope.games = {
 			gameEnded: false,
     		gameBoard: ['','','','','','','','',''],
     		playerTurn: 1
     	};
 
+
+
     	$scope.clickBox = function(box){
-    		if ($scope.games.gameBoard[box] != "X" && $scope.games.gameBoard[box] != "O") 
+    		if ($scope.games[$scope.gameId].gameBoard[box] != "X" && $scope.games[$scope.gameId].gameBoard[box] != "O") 
 	    		{
-	    		$scope.games.gameBoard[box] = ($scope.games.playerTurn % 2 == 1 ? "X" : "O");
+	    		$scope.games[$scope.gameId].gameBoard[box] = ($scope.games[$scope.gameId].playerTurn % 2 == 1 ? "X" : "O");
 	    		}
 			// if($scope.games.gamegameEnded == true) 
 			// return;
@@ -33,10 +47,10 @@ angular.module('newTicApp')
 
 				$scope.win();
 				$scope.tie();
-				$scope.games.playerTurn++;
+				$scope.games[$scope.gameId].playerTurn++;
 
-				console.log($scope.games.gameBoard[box]);
-				console.log($scope.games.gameBoard);
+				console.log($scope.games[$scope.gameId].gameBoard[box]);
+				console.log($scope.games[$scope.gameId].gameBoard);
 
 	  // 	$scope.$watch("games.gameBoard", function(){
    //  		console.log("Game Ended: " + $scope.games.gameEnded);
@@ -58,15 +72,15 @@ angular.module('newTicApp')
 
 		$scope.win = function(){
 				for(var i = 0; i < wins.length; i++)
-				if(($scope.games.gameBoard[wins[i][0]] != '') && 
-					($scope.games.gameBoard[wins[i][0]] == $scope.games.gameBoard[wins[i][1]]) && 
-					($scope.games.gameBoard[wins[i][1]] == $scope.games.gameBoard[wins[i][2]]))
+				if(($scope.games[$scope.gameId].gameBoard[wins[i][0]] != '') && 
+					($scope.games[$scope.gameId].gameBoard[wins[i][0]] == $scope.games[$scope.gameId].gameBoard[wins[i][1]]) && 
+					($scope.games[$scope.gameId].gameBoard[wins[i][1]] == $scope.games[$scope.gameId].gameBoard[wins[i][2]]))
 					{
 						// $scope.showWinMsg = $scope.games.gameBoard[wins[i][0]] + " win! by " + wins[i][3];
-						$scope.games.gameEnded = true;
+						$scope.games[$scope.gameId].gameEnded = true;
 						$scope.showWinMsg = true;
 						// alert($scope.gameBoard[wins[i][0]] + " win! by " + wins[i][3]);
-						console.log($scope.games.gameEnded);
+						console.log($scope.games[$scope.gameId].gameEnded);
 						// This is just as a test...
 						$scope.showWinMsg = true;
 						console.log($scope.showWinMsg);
@@ -77,19 +91,19 @@ angular.module('newTicApp')
 		$scope.tie = function(){
 			var mightBeEnded = true;
 
-			for(var i = 0; i < $scope.games.gameBoard.length; i++)
-				if($scope.games.gameBoard[i]=="")
+			for(var i = 0; i < $scope.games[$scope.gameId].gameBoard.length; i++)
+				if($scope.games[$scope.gameId].gameBoard[i]=="")
 				{
 					mightBeEnded = false;
 					break;
 				}
 
-				$scope.games.gameEnded = mightBeEnded;
+				$scope.games[$scope.gameId].gameEnded = mightBeEnded;
 
-				if($scope.games.gameEnded)
+				if($scope.games[$scope.gameId].gameEnded)
 				{
 					$scope.showTieMsg = true;
-					console.log($scope.games.gameEnded);
+					console.log($scope.games[$scope.gameId].gameEnded);
 					console.log($scope.showTieMsg);
 					// alert("it's a tie!");
 				}
@@ -98,26 +112,70 @@ angular.module('newTicApp')
 
 
 		$scope.resetClick = function(){
-			for(var i = 0; i < $scope.games.gameBoard.length; i++){
-				$scope.games.gameBoard[i] = '';	
+			for(var i = 0; i < $scope.games[$scope.gameId].gameBoard.length; i++){
+				$scope.games[$scope.gameId].gameBoard[i] = '';	
 			}
-			$scope.games.playerTurn = 1;
-			$scope.games.gameEnded = false;
-			console.log($scope.games.gameBoard);
+			$scope.games[$scope.gameId].playerTurn = 1;
+			$scope.games[$scope.gameId].gameEnded = false;
+			console.log($scope.games[$scope.gameId].gameBoard);
 			// alert( "my button was clicked!");
 		};
 
 		$scope.playAgain = function(){
-			for(var i = 0; i < $scope.games.gameBoard.length; i++){
-				$scope.games.gameBoard[i] = '';	
+			for(var i = 0; i < $scope.games[$scope.gameId].gameBoard.length; i++){
+				$scope.games[$scope.gameId].gameBoard[i] = '';	
 			}
 			$scope.showWinMsg = false;
 			$scope.showTieMsg = false;
-			$scope.games.playerTurn = 1;
-			$scope.games.gameEnded = false;
-			console.log($scope.games.gameBoard);
+			$scope.games[$scope.gameId].playerTurn = 1;
+			$scope.games[$scope.gameId].gameEnded = false;
+			console.log($scope.games[$scope.gameId].gameBoard);
 			// alert( "my button was clicked!");
 		};
+
+
+	var queue = new Firebase("https://newtictactoe.firebaseio.com/queue");
+	  	angularFire(queue, $scope, "queue").then(function(){
+	  		if($scope.queue.gameId == undefined)
+	  		{
+	  			console.log("I'm player 1");
+	  			$scope.player = "p1";
+
+	  			var newGame = 
+		  			{
+		  				gameEnded: false,
+		    			gameBoard: ['','','','','','','','',''],
+		    			playerTurn: 1,
+		    			// win:
+		    			// turnCount:
+		  			};
+	  			$scope.games = [];
+	  			console.log($scope.games);
+	  			$scope.gameId = $scope.games.push(newGame) -1;
+	  			$scope.queue.gameId = $scope.gameId;
+	  			console.log("player 1's game is: " + $scope.gameId);
+
+	  			$scope.games.push(newGame);
+
+	  			$scope.queue.gameId = $scope.gameId;
+	  		}
+	  		else
+	  		{
+	  			console.log("I'm player 2");
+	  			$scope.player = "p2";
+
+	  			$scope.gameId = $scope.queue.gameId;
+	  			$scope.queue = {};
+	  			console.log("player 2's game is: " + $scope.gameId);
+	  		}
+
+	  	});
+
+
+
+
+
+
 
 	});
 
